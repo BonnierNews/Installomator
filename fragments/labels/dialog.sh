a382d59359a4ca337f7645dbf39cc8ca99cd7aec
P-M Lejon <pme@mac.com>
2024-04-10 12:18:48 +0200

    Revert "Modify swiftdialog to skip build number from git"

--- fragments/labels/dialog.sh
+++ fragments/labels/dialog.sh
@@ -4,6 +4,6 @@ swiftdialog)
     type="pkg"
     packageID="au.csiro.dialogcli"
     downloadURL="$(downloadURLFromGit swiftDialog swiftDialog)"
-    appNewVersion="$(versionFromGit swiftDialog swiftDialog | awk -F "-" '{print$1}')"
+    appNewVersion="$(versionFromGit swiftDialog swiftDialog)"
     expectedTeamID="PWA5E9TQ59"
     ;;
