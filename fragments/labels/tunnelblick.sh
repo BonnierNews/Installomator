7dcb40a8cb3e8323e5a0ca6b07a4edb5023f608a
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-01-22 08:59:45 +0100

    Correct appNewVersion and add CLIInstaller for Tunnelblick

--- fragments/labels/tunnelblick.sh
+++ fragments/labels/tunnelblick.sh
@@ -1,9 +1,11 @@
 tunnelblick)
     name="Tunnelblick"
     type="dmg"
-    version_plus_build=$(curl -sf https://github.com/Tunnelblick/Tunnelblick/releases  | grep -B2 'Pre-release' |grep -m 1 "/Tunnelblick/Tunnelblick/releases/tag/" | sed -r 's/.*Tunnelblick ([^<]+).*/\1/' | awk '{gsub(/ /,"_"); gsub(/\(|\)/,""); print}')
+    version_plus_build=$(curl -sf https://github.com/Tunnelblick/Tunnelblick/releases  | grep -B2 'Pre-release' |grep -m 1 "/Tunnelblick/Tunnelblick/releases/tag/" | sed -r 's/.*Tunnelblick ([^<]+).*/\1/')
     appNewVersion=$version_plus_build
-    version=$(echo $version_plus_build | awk -F_ '{print $1}')
+    version=$(echo $version_plus_build | awk -F " (" '{print $1}')
     downloadURL="https://github.com/Tunnelblick/Tunnelblick/releases/download/v${version}/Tunnelblick_${version_plus_build}.dmg"
+    CLIInstaller="/Tunnelblick.app/Contents/Resources/installer"
+    CLIArguments="259"
     expectedTeamID="Z2SG5H3HC8"
     ;;
