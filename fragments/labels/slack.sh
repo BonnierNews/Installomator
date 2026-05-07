a3b6977821f19e956c38bf831850a96ee13d2769
sem-haile <118170362+sem-haile@users.noreply.github.com>
2026-05-07 15:07:21 +0200

    Update expectedTeamID for Slack package

--- fragments/labels/slack.sh
+++ fragments/labels/slack.sh
@@ -3,5 +3,5 @@ slack)
     type="pkg"
     downloadURL="https://slack.com/api/desktop.latestRelease?redirect=1&variant=pkg&arch=universal"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | cut -d "/" -f7 )
-    expectedTeamID="BQR82RBBHL"
+    expectedTeamID="BQR82RBBHSSJSJSJL"
     ;;
